339ac416a958d182c879105e158ee5147c5386a7
Ovais Ellahi <oe309@nyu.edu>
2017-11-11 17:53:05 -0500

    created test user

--- pie.py
+++ pie.py
@@ -6,6 +6,7 @@ users = {}
 @app.route("/login/", methods=['post', 'get'])
 def logIn():
     if request.method=='POST':
+        users["test"] = ["123",[],[]]
         username = request.form["username"]
         password = request.form["password"]
 
@@ -23,31 +24,37 @@ def logIn():
         return redirect(url_for('home',username = request.form["username"]));
     return render_template("index.html");
 
+@app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/main')
-def main():
-    return render_template("main.html")
+@app.route('/editprofile')
+def editprofile():
+    return render_template("editProfile.html")
+
 @app.route('/logout/')
 def logOut():
     session.pop('username', None)
     return 'You have logged out.'
 
+@app.route('/main')
+def main():
+    return render_template("main.html")
+
 @app.route('/user/<username>', methods=['post', 'get'])
 def home(username):
     if 'username' not in session: return 'log in first!'
     if request.method=='POST':
-        if "log out" in request.form:
-            return redirect(url_for('logOut'))
+        if "logout" in request.form:
+            return redirect(url_for('index'))
         elif "del acc" in request.form:
             session.pop('username', None)
             del users[username]
             return 'Account deleted.'
         #other functionalities
         
-    return render_template("main.html",user = username);
+    return render_template("editProfile.html", user = username);
 
 app.secret_key = 'A0Zr98j/3yX R~XXH!jN]LWX/,?RT'
 app.run("127.0.0.1", 13000, debug=True)
